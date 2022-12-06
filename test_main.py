abea39a6ad022aee252d8bfaf39c6e98c61deff4
yilmazfirat <yilmazfsalman@gmail.com>
2022-12-06 20:05:20 +0100

    update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,4 +6,8 @@ class TestMain(unittest.TestCase):
 
     def test_add(self):
         result = main.add(4, 5)
-        self.assertEqual(result, 9)
+        self.assertEqual(result, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
