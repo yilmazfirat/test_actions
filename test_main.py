1d8f28f2eec2b06f70f9cc5c7d7b96c83a0fbc35
yilmazfirat <107986062+yilmazfirat@users.noreply.github.com>
2022-12-07 13:38:37 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,8 +5,8 @@ from main import add as main_add
 class TestMain(unittest.TestCase):
 
     def test_add(self):
-        result = main_add(4, 5)
-        self.assertEqual(result, 9)
+        result = main_add(4, 6)
+        self.assertEqual(result, 10)
 
 
 if __name__ == "__main__":
